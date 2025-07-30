314fe5ac2acb578b16620e996a3fef84ba745935
유성 <5emma10003@gmail.com>
2025-07-30 15:49:31 +0900

    Ajax를 활용한 로그인 실습

--- js/12_Ajax_login.js
+++ js/12_Ajax_login.js
@@ -5,7 +5,7 @@
 
 $(function () {
     // 로그인 버튼을 클릭했을 때 ajax를 작동!
-    $("#로그인기능").click(function () {
+    $("#로그인기능").click(function (e) {
         // 1. button 제출 방지
         e.preventDefault(); // submit 잠시 멈춤 -> 정규식이나 비밀번호/아이디 일치하는지 확인하고 제출
         const abc = $("#username").val(); // username 안에 소비자가 작성한 값(id)이 들어있음.
@@ -35,12 +35,15 @@ $(function () {
                 users[abc] -> users[admin] 으로 변경되어 admin과 일치하는 아이디를 검색
                 */
                 if (data.users[abc].password === password) {
+                    // 1. 데이터가 존재할 경우
                     $("#result").html(
                         `로그인 성공! 환영합니다. ${data.users[abc].name} 님`
                     );
                     $("#로그인기능").hide();
                     $("#로그아웃기능").show();
                 } else {
+                    // 2. 데이터가 사용자가 작성한 아이디 비밀번호와 일치하지 않기 때문에
+                    // 데이터를 가져올 수 없을 경우 -> '일치하지 않는 고객이 존재하지 않습니다.'
                     $("#result").html("일치하는 아이디나 비밀번호가 없습니다.");
                 }
             },
@@ -50,10 +53,10 @@ $(function () {
         });
     });
 
-    $("#로그아웃기능").click(function () {
+    $("#로그아웃기능").click(function (e) {
         // 로그아웃을 진행할 경우
-        // 로그인기능 show 보여주기
-        // 로그아웃기능 버튼 hide 숨기기 설정
+        $("#로그아웃기능").hide(); // 로그인기능 show 보여주기
+        $("#로그인기능").show(); // 로그아웃기능 버튼 hide 숨기기 설정
     });
 });
 
