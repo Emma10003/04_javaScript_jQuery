1a016ff4fb679933eca1f5d3c9390faef264ca62
유성 <5emma10003@gmail.com>
2025-07-30 16:07:10 +0900

    Ajax를 활용한 로그인 로그아웃 실습

--- js/12_Ajax_login.js
+++ js/12_Ajax_login.js
@@ -34,17 +34,23 @@ $(function () {
                 예를 들어 username.value 값으로 admin 이 들어왔을 경우
                 users[abc] -> users[admin] 으로 변경되어 admin과 일치하는 아이디를 검색
                 */
-                if (data.users[abc].password === password) {
-                    // 1. 데이터가 존재할 경우
-                    $("#result").html(
-                        `로그인 성공! 환영합니다. ${data.users[abc].name} 님`
-                    );
-                    $("#로그인기능").hide();
-                    $("#로그아웃기능").show();
-                } else {
-                    // 2. 데이터가 사용자가 작성한 아이디 비밀번호와 일치하지 않기 때문에
-                    // 데이터를 가져올 수 없을 경우 -> '일치하지 않는 고객이 존재하지 않습니다.'
-                    $("#result").html("일치하는 아이디나 비밀번호가 없습니다.");
+                if (data.users[abc]) {  // 사용자(아이디)가 존재하는가? 
+                    // 아이디 있음 -> 비밀번호 확인
+                    if (data.users[abc].password === password) { 
+                        // 1. 아이디 비밀번호 모두 존재하는 경우
+                        $("#result").html(
+                            `로그인 성공! 환영합니다. ${data.users[abc].name} 님`
+                        );
+                        $("#로그인기능").hide();
+                        $("#로그아웃기능").show();
+                    } else {
+                        // 2. 아이디가 존재하지만, 비밀번호가 일치하지 않을 때
+                        $("#result").html(
+                            "일치하는 아이디나 비밀번호가 없습니다."
+                        );
+                    }
+                } else { // 3. 아이디와 비밀번호 모두 존재하지 않을 때
+                    $("#result").html("존재하는 아이디가 없습니다.");
                 }
             },
             error: function () {
@@ -55,8 +61,13 @@ $(function () {
 
     $("#로그아웃기능").click(function (e) {
         // 로그아웃을 진행할 경우
-        $("#로그아웃기능").hide(); // 로그인기능 show 보여주기
+        e.preventDefault();  // 로그아웃 button 태그 내부에 type = submit으로 설정되어 있어, 제출 방지.
+            // -> 이걸 안 하는 경우 로그아웃 버튼을 눌렀을 때 창이 새로고침됨.
         $("#로그인기능").show(); // 로그아웃기능 버튼 hide 숨기기 설정
+        $("#로그아웃기능").hide(); // 로그인기능 show 보여주기
+        $("#username").val(""); // 입력창의 값 초기화
+        $("#password").val(""); 
+        $("#result").html("로그아웃이 완료되었습니다."); 
     });
 });
 
