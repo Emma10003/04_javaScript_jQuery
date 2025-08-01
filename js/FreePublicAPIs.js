93c9399b4e7ebaad949d3ecd75518b3a5f344d8c
유성 <5emma10003@gmail.com>
2025-08-01 18:39:30 +0900

    주말과제(학원)

--- js/FreePublicAPIs.js
+++ js/FreePublicAPIs.js
@@ -1,3 +1,22 @@
-$(function() {
-    
-})
+// https://www.thecocktaildb.com/api/json/v1/1/random.php
+
+$(function () {
+    // showInfo();
+    $("#cocktailBtn").click(showInfo);
+});
+
+function showInfo() {
+    $.get("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+        .done(function (data) {
+            $("#contentsResult").html(
+                `
+                <div class="info">
+                    <p>${data.drink[0].strDrink}</p>      // 칵테일 이름
+                    <p>${data.drink[0].strAlcoholic}</p>  // 알코올 있는지 여부
+                    <p>${data.drink[0].strGlass}</p>      // 어떤 잔에 담는지
+                </div>
+                `
+            );
+        })
+        .fail();
+}
